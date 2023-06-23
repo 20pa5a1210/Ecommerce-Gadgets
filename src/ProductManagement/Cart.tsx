3c66090c05e740f2620b9776221e249261c4677a
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-06-23 21:50:08 +0530

    fix: updated cart ui (#3)

--- src/ProductManagement/Cart.tsx
+++ src/ProductManagement/Cart.tsx
@@ -9,17 +9,29 @@ const Cart: React.FC = () => {
   };
 
   return (
-    <div>
-      <h2>Cart</h2>
-      {cartState.cartItems.map((item) => (
-        <div key={item.id}>
-          <h4>{item.name}</h4>
-          <p>Quantity: {item.quantity}</p>
-          <p>Price: ${item.price}</p>
-          <button onClick={() => removeItemFromCart(item.id)}>Remove</button>
-        </div>
-      ))}
-      <button onClick={() => cartDispatch({ type: "CLEAR_CART" })}>
+    <div className="p-4">
+      <h2 className="text-2xl font-bold mb-4">Cart</h2>
+      {cartState.cartItems.map((item) => {
+        const total = item.price * item.quantity;
+        return (
+          <div key={item.id} className="mb-4 p-4 border rounded">
+            <h4 className="text-lg font-semibold">{item.name}</h4>
+            <p className="mb-2">Quantity: {item.quantity}</p>
+            <p className="mb-2">Price: ${item.price}</p>
+            <p className="mb-2">TotalPrice: ${total}</p>
+            <button
+              className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded"
+              onClick={() => removeItemFromCart(item.id)}
+            >
+              Remove
+            </button>
+          </div>
+        );
+      })}
+      <button
+        className="bg-gray-500 hover:bg-gray-600 text-white px-4 py-2 rounded"
+        onClick={() => cartDispatch({ type: "CLEAR_CART" })}
+      >
         Clear Cart
       </button>
     </div>
