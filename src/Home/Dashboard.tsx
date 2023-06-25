a916fff9d948efa38716a422b3f25e68c1c15e6c
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-06-25 15:17:49 +0530

    feat: clear cart api intergrated (#3)

--- src/Home/Dashboard.tsx
+++ src/Home/Dashboard.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useContext, useEffect, useState } from "react";
 import { UserContext } from "./userStore";
-import { Navigate } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 interface UserProfile {
   id: string;
   username: string;
@@ -41,6 +41,23 @@ export default function Dashboard() {
         <div className="container mx-auto px-4">
           <nav className="flex justify-between items-center py-6">
             <div className="text-lg font-bold">Dashboard</div>
+            <Link to="/">
+              <div className="flex items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="black"
+                  className="w-6 h-6"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M6 5v1H4.667a1.75 1.75 0 00-1.743 1.598l-.826 9.5A1.75 1.75 0 003.84 19H16.16a1.75 1.75 0 001.743-1.902l-.826-9.5A1.75 1.75 0 0015.333 6H14V5a4 4 0 00-8 0zm4-2.5A2.5 2.5 0 007.5 5v1h5V5A2.5 2.5 0 0010 2.5zM7.5 10a2.5 2.5 0 005 0V8.75a.75.75 0 011.5 0V10a4 4 0 01-8 0V8.75a.75.75 0 011.5 0V10z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <h1 className="text-black text-2xl font-bold ml-2">ShopCart</h1>
+              </div>
+            </Link>
             <div>
               <button
                 onClick={handleLogout}
--- src/ProductManagement/Cart.tsx
+++ src/ProductManagement/Cart.tsx
@@ -2,14 +2,40 @@ import { useContext, useEffect } from "react";
 import { CartContext } from "./CartStore";
 import { useNavigate } from "react-router-dom";
 import { UserContext } from "../Home/userStore";
+import axios from "axios";
+import { Toaster, toast } from "react-hot-toast";
+import Navbar from "../Home/Navbar";
 
 const Cart = () => {
   const { cartState, cartDispatch } = useContext(CartContext);
   const navigate = useNavigate();
-  const { token } = useContext(UserContext);
+  const { token, username } = useContext(UserContext);
 
   const removeItemFromCart = (itemId: string) => {
     cartDispatch({ type: "REMOVE_ITEM", payload: itemId });
+  };
+  const clearCart = () => {
+    if (token) {
+      axios
+        .delete(`http://localhost:8080/cart/clear/${username}`, {
+          headers: {
+            Authorization: `${token}`,
+          },
+        })
+        .then((res) => {
+          cartDispatch({
+            type: "CLEAR_CART",
+          });
+          cartDispatch({
+            type: "SET_CART",
+            payload: res.data.cartItems,
+          });
+          toast.success("Cart cleared");
+        })
+        .catch((err) => {
+          toast.error(err.response.data.message);
+        });
+    }
   };
   useEffect(() => {
     // Check if user is logged in
@@ -18,32 +44,36 @@ const Cart = () => {
     }
   }, [token, navigate]);
   return (
-    <div className="p-4">
-      <h2 className="text-2xl font-bold mb-4">Cart</h2>
-      {cartState.cartItems.map((item) => {
-        const total = item.price * item.quantity;
-        return (
-          <div key={item.id} className="mb-4 p-4 border rounded">
-            <h4 className="text-lg font-semibold">{item.name}</h4>
-            <p className="mb-2">Quantity: {item.quantity}</p>
-            <p className="mb-2">Price: ${item.price}</p>
-            <p className="mb-2">TotalPrice: ${total}</p>
-            <button
-              className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded"
-              onClick={() => removeItemFromCart(item.id)}
-            >
-              Remove
-            </button>
-          </div>
-        );
-      })}
-      <button
-        className="bg-gray-500 hover:bg-gray-600 text-white px-4 py-2 rounded"
-        onClick={() => cartDispatch({ type: "CLEAR_CART" })}
-      >
-        Clear Cart
-      </button>
-    </div>
+    <>
+      <Navbar />
+      <div className="p-4">
+        <Toaster />
+        <h2 className="text-2xl font-bold mb-4">Cart</h2>
+        {cartState.cartItems.map((item) => {
+          const total = item.price * item.quantity;
+          return (
+            <div key={item.id} className="mb-4 p-4 border rounded">
+              <h4 className="text-lg font-semibold">{item.name}</h4>
+              <p className="mb-2">Quantity: {item.quantity}</p>
+              <p className="mb-2">Price: ${item.price}</p>
+              <p className="mb-2">TotalPrice: ${total}</p>
+              <button
+                className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded"
+                onClick={() => removeItemFromCart(item.id)}
+              >
+                Remove
+              </button>
+            </div>
+          );
+        })}
+        <button
+          className="bg-gray-500 hover:bg-gray-600 text-white px-4 py-2 rounded"
+          onClick={clearCart}
+        >
+          Clear Cart
+        </button>
+      </div>
+    </>
   );
 };
 
